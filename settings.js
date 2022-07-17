affaf6666f2a9955ba314315a2b045a0cc33fdc9
whatsappsanu <109449336+whatsappsanu@users.noreply.github.com>
2022-07-16 21:53:43 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,31 +19,31 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is 🔥Dαɾƙ ꪜ𝔖𝔞𝔫𝔲 ʷᵃBOT💘
+My Creator is 😍𝙀.𝘿.𝙎.𝙈𝙤𝙨𝙝𝙞𝙩𝙝𝙖💘` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94774309804'] //Owner number in aive msg
+global.premium = ['+94774309804'] //Owner Number info
+global.ownernomer = '+94774309804' //Owner Number <<<
 
 global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.botname = ' ☯Dαɾƙ ꪜ𝔖𝔞𝔫𝔲 ʷᵃBOT ' //Bot Name
 
-global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.button = '😍මේක click කරන්න.💘' //Costomize A Button Name In Alive Message
+global.btnurl = 'දැන් සැපද 🤣' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☯Dαɾƙ ꪜ𝔖𝔞𝔫𝔲 ʷᵃBOT ' //Bot Pacage Name
+global.author = '😍𝙀.𝘿.𝙎.𝙈𝙤𝙨𝙝𝙞𝙩𝙝𝙖💘❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_☯Dαɾƙ ꪜ𝔖𝔞𝔫𝔲 ʷᵃBOT .' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://youtube.com/channel/UCtKT-h1UkEJ5sDJ3Std270g' //youtube Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
@@ -54,7 +54,7 @@ global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
     group: 'Feature Used Only For Groups!',
     private: 'This Features Only For Private Chat!',
     bot: 'This Feature Is Only For Bot😝',
-    wait: '👉👈Pleas Wait...',
+    wait: '👉☯Dαɾƙ ꪜ𝔖𝔞𝔫𝔲 ʷᵃBOT 👈Pleas Wait...',
     error: 'Error!!! Maybe Api Key Is Expired🤔!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 }
